b5c2f533a2850d8e65edfb0e89ef7c3166a5fdfd
Michael Gerasymenko <mihail@gerasimenko.me>
2023-06-22 10:48:23 +0200

    Add shared library target for Carthage

---
+++ Rating Demo/FloatRatingView/FloatRatingView.h
@@ -0,0 +1,19 @@
+//
+//  FloatRatingView.h
+//  FloatRatingView
+//
+//  Created by Michael Gerasymenko on 22.06.23.
+//  Copyright © 2023 On The Pursuit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FloatRatingView.
+FOUNDATION_EXPORT double FloatRatingViewVersionNumber;
+
+//! Project version string for FloatRatingView.
+FOUNDATION_EXPORT const unsigned char FloatRatingViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FloatRatingView/PublicHeader.h>
+
+
--- Rating Demo/Rating Demo/ViewController.swift
+++ Rating Demo/Rating Demo/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FloatRatingView
 
 class ViewController: UIViewController {
     
